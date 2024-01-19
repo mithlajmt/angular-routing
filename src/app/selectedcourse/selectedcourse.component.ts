ca1f856649ec2fe8b95c8d6b77077925267b8270
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-01-19 15:11:26 +0530

    route parameters setted

--- src/app/selectedcourse/selectedcourse.component.ts
+++ src/app/selectedcourse/selectedcourse.component.ts
@@ -1,26 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CourseService } from '../services/courses.service';
 
 @Component({
   selector: 'app-selectedcourse',
   templateUrl: './selectedcourse.component.html',
-  styleUrls: ['./selectedcourse.component.css']
+  styleUrls: ['./selectedcourse.component.css'],
+  providers:[CourseService]
+
 })
 export class SelectedcourseComponent implements OnInit {
   
   idnum!:number;
+  selectedCourse:any
   
   // Inject ActivatedRoute through the constructor
-  constructor(private router: ActivatedRoute) { }
+  constructor(private router: ActivatedRoute, private courses:CourseService) { }
   
   ngOnInit(): void {
     // Use optional chaining to handle the possibility of null or undefined
-    this.idnum = Number( this.router?.snapshot.paramMap.get('id'))
+    this.idnum = Number(this.router?.snapshot.paramMap.get('id'));
     console.log(this.idnum);
+  
+    // Corrected assignment of selectedCourse
+    this.selectedCourse = this.courses.courses.find((data) => data.id === this.idnum);
+  }
+  
     
     
     // if (idParam !== null && idParam !== undefined) {
       // this.idnum = +idParam || 0;
     // }
   }
-}
+
