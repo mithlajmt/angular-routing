c6ea66235f030bfafb55e13a76445ebd711f1f24
mithlajmatta <144442116+mithlajmatta@users.noreply.github.com>
2024-01-22 21:50:03 +0530

    reactiveform has added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'templateform';
-}
+  // @ViewChild('fname') firstNameInput: any; // Change 'any' to the appropriate type if know
+  firstname:string=''
+  lastname:string=''
+  email:string=''
+  onFormSubmit(data:any){
+    console.log(data);
+    
+  }
+
+  }
+  
+
